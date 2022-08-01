3505c47ee4c8490526009c4aa0e30c2f7ab5562b
wxd <wxdwfc@sjtu.edu.cn>
2022-08-01 11:15:11 +0800

    put back to shim

---
+++ rdma-shim/src/user/bindings.rs
@@ -0,0 +1,1 @@
+pub use rust_user_rdma::*;
--- rdma-shim/src/user/mod.rs
+++ rdma-shim/src/user/mod.rs
@@ -1,11 +1,11 @@
-use rust_user_rdma::*;
+pub mod bindings;
 
 #[cfg(test)]
 mod tests {
     #[test]
     fn has_rdma() {
         let mut n = 0i32;
-        let devices = unsafe { super::ibv_get_device_list(&mut n as *mut _) };
+        let devices = unsafe { super::bindings::ibv_get_device_list(&mut n as *mut _) };
         
         // should have devices
         assert!(!devices.is_null());    
