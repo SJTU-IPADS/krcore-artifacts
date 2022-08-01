cdd52224d1b15592f8794d57df0c7c2cc5b6e515
wxd <wxdwfc@sjtu.edu.cn>
2022-08-01 11:13:34 +0800

    put back to RDMA-shim

--- rdma-shim/src/user/mod.rs
+++ rdma-shim/src/user/mod.rs
@@ -1,10 +1,13 @@
-use rdma_sys::*;
+use rust_user_rdma::*;
 
 #[cfg(test)]
 mod tests {
     #[test]
-    fn rdma_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn has_rdma() {
+        let mut n = 0i32;
+        let devices = unsafe { super::ibv_get_device_list(&mut n as *mut _) };
+        
+        // should have devices
+        assert!(!devices.is_null());    
     }
 }
